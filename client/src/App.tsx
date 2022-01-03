fa56c738069119ed1e96ae3931c1379e8d460417
Evan Yang <evanjyang4@gmail.com>
2022-01-03 15:45:27 -0800

    fixed issue with typescript

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,5 +1,6 @@
 import "./App.css";
-import React, { FunctionComponent } from "react";
+import * as React from "react";
+import { FunctionComponent } from "react";
 
 const App: FunctionComponent = () => {
   return <div className="App"></div>;
