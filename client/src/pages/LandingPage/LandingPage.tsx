2739bd0d555b4c12aa3b156e47042bec883d6c14
Evan Yang <evanjyang4@gmail.com>
2022-01-05 13:45:17 -0800

    landing page updates with login form

--- client/src/pages/LandingPage/LandingPage.tsx
+++ client/src/pages/LandingPage/LandingPage.tsx
@@ -1,14 +1,32 @@
+import "./LandingPage.css";
 import "bulma/css/bulma.min.css";
 import * as React from "react";
 import { FunctionComponent } from "react";
-import { Button, Container, Heading } from "react-bulma-components";
+import { Button, Form, Heading } from "react-bulma-components";
+
+const { Input, Field, Control, Label } = Form;
 
 const LandingPage: FunctionComponent = () => {
   return (
-    <Container>
+    <div className="div1">
       <Heading size={1}>BlogChat</Heading>
-      <Button>hi</Button>
-    </Container>
+      <div className="login">
+        <Field>
+          <Label>Username</Label>
+          <Control>
+            <Input placeholder="e.g. John Doe" type="text" />
+          </Control>
+        </Field>
+        <Field>
+          <Label>Password</Label>
+          <Control>
+            <Input placeholder="password" type="password" />
+          </Control>
+        </Field>
+
+        <Button>Log in</Button>
+      </div>
+    </div>
   );
 };
 
